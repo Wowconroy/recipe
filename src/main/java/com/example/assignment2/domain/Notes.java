c4b938d10222c606c2d1cd9fd916ec572c464343
Oleksii <shostak.ok@gmail.com>
2022-05-10 22:06:55 +0300

    Create 2 POJOs + Index Controller

---
+++ src/main/java/com/example/assignment2/domain/Notes.java
@@ -0,0 +1,7 @@
+package com.example.assignment2.domain;
+
+public class Notes {
+    private Recipe recipe;
+    private String recipeNote;
+
+}
---
+++ src/main/java/com/example/assignment2/domain/Recipe.java
@@ -0,0 +1,13 @@
+package com.example.assignment2.domain;
+
+public class Recipe {
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    private Byte[] image;
+}
