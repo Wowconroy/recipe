c0bdd8349952230e262e6f9a59c768eeb81d8d64
Oleksii <shostak.ok@gmail.com>
2022-05-11 12:07:19 +0300

    Create Category.java and add relationship @ManyToMany

--- src/main/java/com/example/assignment2/domain/Recipe.java
+++ src/main/java/com/example/assignment2/domain/Recipe.java
@@ -23,6 +23,12 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id    "))
+    private Set<Category> categories = new HashSet<>();
+
     @Enumerated(EnumType.STRING)
     private Difficulty difficulty;
 
@@ -126,5 +132,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
